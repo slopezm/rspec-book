d2977292a79b2a90a702d6db8183d00f59757518
Sergio Lopez <wapankh@never-forget.lan>
2012-12-21 22:13:56 -0600

    End of chapter 9

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -10,24 +10,8 @@ module Codebreaker
 		end
 
 		def guess(guess)
-			mark_plus = ''
-			mark_minus = ''
-			(0..3).each do |index|
-				if exact_match?(guess,index)
-					mark_plus << "+"
-				elsif number_match?(guess,index)
-					mark_minus << '-'
-				end				
-			end
-			@output.puts mark_plus << mark_minus
-		end
-
-		def exact_match?(guess,index)
-			guess[index] == @secret[index]
-		end
-
-		def number_match?(guess,index)
-			@secret.include?(guess[index]) && !exact_match?(guess,index)
+			@marker = Marker.new(@secret, guess)
+			@output.puts '+'*@marker.exact_match_count + '-'*@marker.number_match_count
 		end
 	end
 end
